005baab0a0141dc5f46eb4b6a1acd833a05b5d7b
AleksandrSterne <lazdju@gmail.com>
2023-05-11 01:14:06 +0200

    Created Header and Layout components

---
+++ components/layout/SidebarItem.tsx
@@ -0,0 +1,29 @@
+import { IconType } from 'react-icons';
+
+interface SidebarItemProps {
+    label: string;
+    href?: string;
+    icon: IconType;
+    onClick?: () => void;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+    label,
+    href,
+    icon: Icon,
+    onClick,
+}) => {
+    return (
+        <div className="flex flex-row items-center">
+            <div className="relative flex items-center justify-center p-4 rounded-full cursor-pointer h-14 w-14 hover:bg-slate-300 hover:bg-opacity-10 lg:hidden">
+                <Icon size={28} color="white" />
+            </div>
+            <div className="relative items-center hidden gap-4 p-4 rounded-full cursor-pointer lg:flex hover:bg-slate-300 hover:bg-opacity-10">
+                <Icon size={24} color="white" />
+                <p className="hidden text-xl text-white lg:block">{label}</p>
+            </div>
+        </div>
+    );
+};
+
+export default SidebarItem;
